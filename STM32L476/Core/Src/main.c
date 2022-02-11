a857f776dfa29a5fbdfddfaf34bc930e49ee0a47
jernejp21 <jernej.pangerc@tutamail.com>
2022-02-11 09:55:34 +0900

    One more test

--- STM32L476/Core/Src/main.c
+++ STM32L476/Core/Src/main.c
@@ -56,7 +56,7 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int delayTime = 3000;
+int delayTime = 2000;
 
 /* USER CODE END 0 */
 
